f56fac5d702b19dbcbe0266b7fb285207cd3687c
Rahul Kumar <128629336+R0ahulkd@users.noreply.github.com>
2026-09-25 12:42:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const byteSize = (str) => {
 	return new Blob([str]).size;
 };
 
-// Do not change the code below
-// const str = prompt("Enter some string.");
-// alert(byteSize(str));
+Do not change the code below
+const str = prompt("Enter some string.");
+alert(byteSize(str));
